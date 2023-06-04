feaeecf4bac28dc15d730594e7022ce5e7ac6f5f
Andres Alarcon <aalarcon@monoku.com>
2023-06-04 14:26:03 -0500

    chore(docs): Add vercel integration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
   import '../app.css';
   import '../theme.css';
+
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
